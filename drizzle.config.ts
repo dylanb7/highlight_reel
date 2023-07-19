a577ea93d1325e6ec814a118697fa31afb5fdc36
Dylan Baker <dylanbaker7@gmail.com>
2023-07-19 18:30:54 +0200

    removed prisma and nextauth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,9 @@
 import { Config } from "drizzle-kit";
 
 export default {
-  connectionString:
-    'mysql://j5qb8xs62jxemhn3w141:pscale_pw_HgXj7mBZJtWhY5SUhf6QZmiE5Pkv3KKlC7rpMxQ0pFI@aws.connect.psdb.cloud/highlight_reel?ssl={"rejectUnauthorized":true}',
+  database: "highlight_reel",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   schema: "src/server/db/schema.ts",
 } satisfies Config;
