18b8d03449931ce580fffd6fbc21284e951f55f4
Dylan Baker <dylanbaker7@gmail.com>
2023-07-19 20:11:01 +0200

    g

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,49 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["isaacscript", "import"],
+  extends: [
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: [
+      "./tsconfig.json",
+      "./cli/tsconfig.eslint.json", // separate eslint config for the CLI since we want to lint and typecheck differently due to template files
+      "./upgrade/tsconfig.json",
+      "./www/tsconfig.json",
+    ],
+  },
+  overrides: [
+    // Template files don't have reliable type information
+    {
+      files: ["./cli/template/**/*.{ts,tsx}"],
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
+  ],
+  rules: {
+    // These off/not-configured-the-way-we-want lint rules we like & opt into
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            files: ['*.ts', '*.tsx'], 
-            extends: [
-              'plugin:@typescript-eslint/recommended',
-              'plugin:@typescript-eslint/recommended-requiring-type-checking',
-            ],
-            parserOptions: {
-            project: ['./tsconfig.json'], 
-            },
-        }
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    "import/consistent-type-specifier-style": ["error", "prefer-inline"],
+
+    // For educational purposes we format our comments/jsdoc nicely
+    "isaacscript/complete-sentences-jsdoc": "warn",
+    "isaacscript/format-jsdoc-comments": "warn",
+
+    // These lint rules don't make sense for us but are enabled in the preset configs
+    "@typescript-eslint/no-confusing-void-expression": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+  },
 }
