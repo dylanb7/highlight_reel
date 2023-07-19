ff7f99d23e0f15bd6b2e61bbe9bf04104ca2a855
Dylan Baker <dylanbaker7@gmail.com>
2023-07-19 20:04:05 +0200

    k

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,9 @@ module.exports = {
               'plugin:@typescript-eslint/recommended',
               'plugin:@typescript-eslint/recommended-requiring-type-checking',
             ],
-      
-            
+            parserOptions: {
+            project: ['./tsconfig.json'], 
+            },
         }
     ],
     "parserOptions": {
